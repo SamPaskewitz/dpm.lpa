a54af17ea7ccfd5a1245d3995be97cec9080e506
SamPaskewitz <55294273+SamPaskewitz@users.noreply.github.com>
2024-06-06 11:58:44 -0400

    various updates

--- R/wrappers for Python code.R
+++ R/wrappers for Python code.R
@@ -10,7 +10,11 @@ fit_dpm_lpa = function(x,
                        seed = 1234,
                        tolerance = 1e-05,
                        max_iter = 50,
-                       min_iter = 30){
+                       min_iter = 30,
+                       standardize_data = TRUE){
+  if(standardize_data){
+    x = as.data.frame(scale(x))
+  }
   result = lpa$fit_lpa$fit_lpa(x, as.integer(n_workers), kmeans, as.integer(restarts), as.integer(truncation_level), prior_w1, prior_w2, prior_lambda_mu, prior_strength_for_xi, as.integer(seed), tolerance, as.integer(max_iter), as.integer(min_iter))
   best_model = result$best_model
   best_model$x_names_R = colnames(x) # there's some SNAFU about accessing best_model.x_names in R
@@ -62,9 +66,6 @@ print_lpa_summary = function(model,
 
 outcome_analysis_normal = function(model,
                                    y,
-                                   x_new = NULL,
-                                   y_new = NULL,
-                                   index = NULL,
                                    profile_labels = NULL,
                                    do_post_hoc = TRUE,
                                    start_profile_labels_from1 = TRUE,
@@ -75,12 +76,17 @@ outcome_analysis_normal = function(model,
                                    figure_size = c(5, 6),
                                    font_size = 11,
                                    facet_var = 'variable',
-                                   ncol = 4){
-  result = lpa$lpa_outcome_analysis$fit_y_normal(model, y, x_new, y_new, index, profile_labels, do_post_hoc, start_profile_labels_from1, prior_alpha, prior_beta, prior_m, prior_lambda, figure_size, font_size, facet_var, ncol)
+                                   ncol = 4,
+                                   standardize_data = TRUE){
+  if(standardize_data){
+    y = as.data.frame(scale(y))
+  }
+  result = lpa$lpa_outcome_analysis$fit_y_normal(model, y, profile_labels, do_post_hoc, start_profile_labels_from1, prior_alpha, prior_beta, prior_m, prior_lambda, figure_size, font_size, facet_var, ncol)
   rownames(result$bayes_factors) = colnames(y) # for some reason, the variable names are dropped, so I need to add them back in
   result$bayes_factors$bf10 = round(as.numeric(result$bayes_factors$bf10), 2) # need to fix column types to get things to display properly
   result$bayes_factors$log10_bf10 = round(as.numeric(result$bayes_factors$log10_bf10), 2)
   result$bayes_factors$post_hoc_result = as.character(result$bayes_factors$post_hoc_result)
+  print(result$bayes_factors)
   return(result)
 }
 
--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,17 @@
 lpa <- NULL
 
-.onLoad <- function(libname, pkgname) {
+.onLoad <- function(libname, pkgname) { # This is run every time the R package is loaded.
+  try(reticulate::install_miniconda(), silent = TRUE) # Install Miniconda if it isn't already installed.
   if(!('r-dpm_lpa' %in% reticulate::conda_list()$name)){
+    # If it doesn't already exist, create the r-dpm_lpa conda environment and
+    # import necessary Python packages. This should only happen the first time the
+    # R package is loaded.
     reticulate::conda_create(envname = 'r-dpm_lpa',
                              packages = c('numpy', 'scipy', 'pandas'))
     reticulate::conda_install(envname = 'r-dpm_lpa',
                               packages = c('vbayesfa', 'plotnine'),
                               pip = TRUE)
   }
-  reticulate::use_condaenv('r-dpm_lpa')
-  lpa <<- reticulate::import('vbayesfa')
+  reticulate::use_condaenv('r-dpm_lpa') # Activate the r-dpm_lpa conda environment.
+  lpa <<- reticulate::import('vbayesfa') # Import the vbayesfa Python package.
 }
